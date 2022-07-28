dd5e33d37974730b60286b4ce39b313dbd9f7090
Enes-Ironhack <enes1.koc@epitech.eu>
2022-07-28 17:37:01 +0200

    Lab solution

---
+++ week1/labs/lab-react-tweets/src/components/Tweet.js
@@ -0,0 +1,29 @@
+import Actions from './Actions';
+import Message from './Message';
+import ProfileImage from './ProfileImage';
+import Timestamp from './Timestamp';
+import User from './User';
+
+function Tweet(props) {
+  return (
+    <div className="tweet">
+      <ProfileImage image={props.tweet.user.image} />
+
+      <div className="body">
+        <div className="top">
+          <User userData={props.tweet.user} />
+
+          <Timestamp time={props.tweet.timestamp} />
+        </div>
+
+        <Message message={props.tweet.message} />
+
+        <Actions />
+      </div>
+
+      <i class="fas fa-ellipsis-h"></i>
+    </div>
+  );
+}
+
+export default Tweet;
