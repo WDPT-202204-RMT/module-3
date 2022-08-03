23bfa4c8a631b342fc3f6c9786ec4758c2f1f6e1
Matteo Gassend <mgassend@gmail.com>
2022-08-03 14:01:08 +0200

    lab v2

--- week2/day3/lab-training/src/components/Dice.js
+++ week2/day3/lab-training/src/components/Dice.js
@@ -1,20 +1,29 @@
 import { useEffect, useState } from 'react';
+import dice0 from '../assets/images/dice-empty.png'
+import dice1 from '../assets/images/dice1.png'
+import dice2 from '../assets/images/dice2.png'
+import dice3 from '../assets/images/dice3.png'
+import dice4 from '../assets/images/dice4.png'
+import dice5 from '../assets/images/dice5.png'
+import dice6 from '../assets/images/dice6.png'
 
 export default function Dice() {
   const [picture, setPicture] = useState();
+  const dices=[dice1, dice2, dice3, dice4, dice5, dice6]
+
 
   useEffect(() => {
     const value = Math.floor(Math.random() * (6 - 1 + 1) + 1);
-    setPicture(`dice${value}`);
+    setPicture(dices[value]);
   }, []);
 
   const roll = () => {
-    setPicture('dice-empty.png');
+    setPicture(dice0);
     setTimeout(() => {
       const value = Math.floor(Math.random() * (6 - 1 + 1) + 1);
-      setPicture(`dice${value}`);
+      setPicture(dices[value]);
     }, 1000);
   };
 
-  return <img src={`../assets/images/${picture}.png`} onClick={() => roll()} />;
+  return <img src={picture} onClick={() => roll()} />;
 }
