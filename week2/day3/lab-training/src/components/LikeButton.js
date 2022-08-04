9739d8c9725a18ea853ad7393d8749e2cd2e1743
Matteo Gassend <mgassend@gmail.com>
2022-08-04 21:29:17 +0200

    w2d3labEx8

--- week2/day3/lab-training/src/components/LikeButton.js
+++ week2/day3/lab-training/src/components/LikeButton.js
@@ -1,11 +1,26 @@
 import { useState } from 'react';
 
+const colors = ['purple', 'blue', 'green', 'yellow', 'orange', 'red'];
+
 export default function LikeButton() {
   const [counter, setCount] = useState(0);
+  const [colorIndex, setColorIndex] = useState(0);
 
   return (
     <div>
-      <button onClick={() => setCount(counter + 1)}>{counter} Likes</button>
+      <button
+        style={{ backgroundColor: colors[colorIndex] }}
+        onClick={() => {
+          setCount(counter + 1);
+          if (colorIndex === colors.length - 1) {
+            setColorIndex(0);
+          } else {
+            setColorIndex(colorIndex + 1);
+          }
+        }}
+      >
+        {counter} Likes
+      </button>
     </div>
   );
 }
